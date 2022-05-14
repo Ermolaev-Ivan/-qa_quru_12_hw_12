9c5332602b91cd5c6df8577da5cdbaf42df844dd
ErmolaevIvan <thedollmaster@bk.ru>
2022-05-14 11:32:18 +0300

    first commit

---
+++ src/test/java/ivan/ermolaev/test/TestBase.java
@@ -0,0 +1,48 @@
+package ivan.ermolaev.test;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import ivan.ermolaev.Config.CredentialsConfig;
+import ivan.ermolaev.helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
+
+public class TestBase {
+
+    @BeforeAll
+    static void setUp() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        CredentialsConfig config = ConfigFactory.create(CredentialsConfig.class);
+
+        //в jenkins прописанно создание файла с конфигом
+        String login = config.login();
+        String password = config.password();
+        String urlSelenode = System.getProperty("urlSelenode");
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("version", "100");
+        Configuration.browserSize = System.getProperty("browserSize", "2560x1440");
+        Configuration.remote = "https://" + login + ":" + password +"@" + urlSelenode;
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        capabilities.setBrowserName("chrome");
+        Configuration.browserCapabilities = capabilities;
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+        closeWebDriver();
+    }
+}
